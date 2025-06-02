694e2a3cecc76f813b493dfa2f1ab36c3bb6ae81
juniorsmartins <junior.soares.martins@proton.me>
2025-06-02 11:43:57 -0300

    First Commit

---
+++ src/main/java/com/agrotis_2025/Agrotis2025Application.java
@@ -0,0 +1,13 @@
+package com.agrotis_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agrotis2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agrotis2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrotis_2025/Agrotis2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotis_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agrotis2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
