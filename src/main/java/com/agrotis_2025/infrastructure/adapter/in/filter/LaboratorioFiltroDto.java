4d54fb560064cddf123833c0cd0cf1c0ae5925cf
juniorsmartins <junior.soares.martins@proton.me>
2025-06-05 16:41:30 -0300

    ...

--- src/main/java/com/agrotis_2025/infrastructure/adapter/in/filter/LaboratorioFiltroDto.java
+++ src/main/java/com/agrotis_2025/infrastructure/adapter/in/filter/LaboratorioFiltroDto.java
@@ -3,11 +3,11 @@ package com.agrotis_2025.infrastructure.adapter.in.filter;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(name = "LaboratorioFiltroDto", description = "Transportador de dados de saída.")
+@Schema(name = "LaboratorioFiltroDto", description = "Transportador de dados de entrada.")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record LaboratorioFiltroDto(
 
-        @Schema(name = "propriedadeId", description = "Identificador único do recurso.",
+        @Schema(name = "laboratorioId", description = "Identificador único do recurso.",
                 example = "034eb74c-69ee-4bd4-a064-5c4cc5e9e748")
         String laboratorioId,
 
